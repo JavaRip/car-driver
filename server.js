3b3f5c712f9a01267a17054d75e1f3191d550f53
Jack Travis <up938828@myport.ac.uk>
2022-04-02 13:01:54 +0100

    serverside

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 import express from 'express';
+import fs from 'fs';
 const app = express();
 
-app.use(express.static('client'));
 const listeningServer = app.listen(8080);
+
+function asyncWrap(f) {
+  return (req, res, next) => {
+    Promise.resolve(f(req, res, next))
+      .catch((e) => next(e || new Error()));
+  };
+}
+
+export async function sendGSToServer(GS) {
+    const data = GS.values()
+    await fs.appendFile(`data.csv`, data, 'utf8')
+}
+
+app.use(express.static('client'));
+app.post('/gs', express.json(), asyncWrap(writeGSToCSV()))
 
